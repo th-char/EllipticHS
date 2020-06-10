cc806a79d3fff53e19b40186ab1229ab83517c9c
Theo <tc4618@ic.ac.uk>
2020-06-10 14:54:16 +0100

    typo

--- src/Main.hs
+++ src/Main.hs
@@ -88,11 +88,11 @@ settings = defaultConfig { resamples = 5
 main = mapM_ benchFactorisation tests
   where 
     tests = [ 2253907771
-            , 34090746634016945057
-            , 2192392375347842185105699
+            --, 34090746634016945057
+            --, 2192392375347842185105699
             , 408690345942924410509381586819
-            , 37474679579109250157949172138642181
-            , 52635022169628958833592981554606658319
+            --, 37474679579109250157949172138642181
+            --, 52635022169628958833592981554606658319
             ]
 
 
@@ -101,19 +101,19 @@ benchFactorisation :: Integer -> IO ()
 benchFactorisation n = do 
      putStrLn $ "Factorising " ++ (show n)
  
-     -- putStrLn $ "    With Pollards:"
-     -- t <- getCurrentTime
-     -- putStrLn $ "       Result: " ++ (formatResult $ pollards n 50000000)
-     -- putStrLn $ "       Result: " ++ (formatResult $ pollards n 50000000)
-     -- putStrLn $ "       Result: " ++ (formatResult $ pollards n 50000000)
-     -- t' <- getCurrentTime
-     -- putStrLn $ "   Time: " ++ (show $ (diffUTCTime t' t) / 3)
+     putStrLn $ "    With Pollards:"
+     t <- getCurrentTime
+     putStrLn $ "       Result: " ++ (formatResult $ pollards n 50000000)
+     putStrLn $ "       Result: " ++ (formatResult $ pollards n 50000000)
+     putStrLn $ "       Result: " ++ (formatResult $ pollards n 50000000)
+     t' <- getCurrentTime
+     putStrLn $ "   Time: " ++ (show $ (diffUTCTime t' t) / 3)
  
      putStrLn $ "  With Lenstras:"
      t <- getCurrentTime
-     putStrLn $ "       Result: " ++ (formatResult $ runLenstra (mkStdGen 25) n)
-     putStrLn $ "       Result: " ++ (formatResult $ runLenstra (mkStdGen 25) n)
-     putStrLn $ "       Result: " ++ (formatResult $ runLenstra (mkStdGen 25) n)
+     putStrLn $ "       Result: " ++ (formatResult $ runLenstra (mkStdGen 42) n)
+     putStrLn $ "       Result: " ++ (formatResult $ runLenstra (mkStdGen 42) n)
+     putStrLn $ "       Result: " ++ (formatResult $ runLenstra (mkStdGen 42) n)
      t' <- getCurrentTime
      putStrLn $ "   Time: " ++ (show $ (diffUTCTime t' t) / 3)
    where 
